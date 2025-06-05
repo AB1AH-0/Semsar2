a87d0f5a76e6ed040cb8a678e6cb2d4d5ab2b6c2
Shaimaa77266 <shaimaa.mohamed.mahmoud2003@gmail.com>
2025-06-05 22:50:06 +0300

    باك اند

--- myproject/inquiries/admin.py
+++ myproject/inquiries/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 from .models import Inquiry
 
 # Register your models here.
-admin.site.register(Inquiry)
+class InquiryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'transaction_type', 'city', 'area', 'property_type', 'created_at')
+    list_filter = ('transaction_type', 'property_type', 'city', 'created_at')
+    search_fields = ('city', 'area', 'property_type')
+    readonly_fields = ('created_at',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('transaction_type', 'city', 'area', 'property_type')
+        }),
+        ('Details', {
+            'fields': ('bedrooms', 'bathrooms', 'min_price', 'max_price', 'min_size', 'max_size', 'furnished')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at',)
+        }),
+    )
+
+admin.site.register(Inquiry, InquiryAdmin)
