c20489f627be2d27354af9d6d494c52efabd54ca
Shaimaa77266 <shaimaa.mohamed.mahmoud2003@gmail.com>
2025-06-05 23:08:30 +0300

    باك اند

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
 
     # Front-end pages (if you’re just serving home.html and index.html as static templates)
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('brokers/', TemplateView.as_view(template_name='Brokers.html'), name='brokers'),
+    path('brokers/', TemplateView.as_view(template_name='brokers.html'), name='brokers'),
 ]
 
 if settings.DEBUG:
